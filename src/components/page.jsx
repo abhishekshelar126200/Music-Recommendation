71ab9cd69f19e9e1b7e01dcdc0601ae00d44c542
Abhishek Balkrishna Shelar <116205134+abhishekshelar126200@users.noreply.github.com>
2024-07-30 10:03:28 +0530

    Update page.jsx

--- src/components/page.jsx
+++ src/components/page.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState,useEffect } from 'react'
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 import { zip } from 'lodash';
 function Page({song}){
