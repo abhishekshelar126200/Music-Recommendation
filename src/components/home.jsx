092badad9f9259f96d8d0a037c868cfcf188ee5c
Abhishek Balkrishna Shelar <116205134+abhishekshelar126200@users.noreply.github.com>
2024-10-20 01:10:56 +0530

    Add files via upload

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -9,7 +9,7 @@ function Home(){
     const [inputValue, setInputValue] = useState(''); 
     const [data, setData] = useState([[],[]]);
     const [isLoading,setLoading]=useState(false)
-     
+    
     const {musicName}=useParams();
     const playMusic=async (songName,poster)=>{
         localStorage.setItem('songName',songName)
@@ -64,7 +64,7 @@ function Home(){
                     <div className="o_card flex mt-2 flex-wrap justify-between">
                         {
                             zip(data[0],data[1]).map(([nam,poster],index)=>{
-                            return <Link to="https://abhishekshelar126200.github.io/Music-Recommendation/songs"><div key={index} className="card relative p-2 rounded-md mb-3 header hover:bg-[#313131] hover:w-[10.1rem] group" onClick={()=>playMusic(nam,poster)} >
+                            return <Link to="/songs"><div key={index} className="card relative p-2 rounded-md mb-3 header hover:bg-[#313131] hover:w-[10.1rem] group" onClick={()=>playMusic(nam,poster)} >
                                     <div  className="play rounded-full w-10 absolute right-4 bottom-10 opacity-0 flex justify-center p-2  bg-green-400 group-hover:opacity-100 transition-opacity duration-1000">
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="15" height="24" color="#000000" fill="black">
                                             <path d="M18.8906 12.846C18.5371 14.189 16.8667 15.138 13.5257 17.0361C10.296 18.8709 8.6812 19.7884 7.37983 19.4196C6.8418 19.2671 6.35159 18.9776 5.95624 18.5787C5 17.6139 5 15.7426 5 12C5 8.2574 5 6.3861 5.95624 5.42132C6.35159 5.02245 6.8418 4.73288 7.37983 4.58042C8.6812 4.21165 10.296 5.12907 13.5257 6.96393C16.8667 8.86197 18.5371 9.811 18.8906 11.154C19.0365 11.7084 19.0365 12.2916 18.8906 12.846Z" stroke="currentColor" stroke-width="1.5" stroke-linejoin="round" />
--- src/components/songs.jsx
+++ src/components/songs.jsx
@@ -28,10 +28,10 @@ function Songs() {
             const details={nam:'',desc:'',isSet:false}
             
            if(!details.isSet){
-                const urlResponse1=await fetch(process.env.NOCODEAPI_PLAYLIST)
+                const urlResponse1=await fetch(`https://v1.nocodeapi.com/abbyshek/spotify/SDyfMiztifIURTOV/search?q=${localStorage.getItem('songName')}&type=playlist`)
                 const convertedResponse1=await urlResponse1.json() 
     
-                const urlResponse=await fetch(process.env.NOCODEAPI_TRACK)
+                const urlResponse=await fetch(`https://v1.nocodeapi.com/abbyshek/spotify/SDyfMiztifIURTOV/search?q=${localStorage.getItem('songName')}&type=track`)
                 const convertedResponse=await urlResponse.json()
                 console.log(convertedResponse.tracks.items)
                 localStorage.setItem('songs',JSON.stringify(convertedResponse.tracks.items))
@@ -91,7 +91,7 @@ function Songs() {
         if (isPause) {
             audioRef.current.play();
         } 
-    },[playsong])
+    },[currentSong])
 
 
     const playmusic=()=>{
